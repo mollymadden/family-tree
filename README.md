439078e93d1fc4bdce90de8eb07cb5db3a716b9c
Molly Madden <molly.annette.madden@gmail.com>
2019-09-05 22:03:27 +1000

    error handling requires more work

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 ## Documentation
+
+in order to run the file you will need the following gem
+> 'tty-prompt'
+
+download and unzip file titled Molly-Madden-Family-Tree.zip
+read through this README.md thoroughly
+
+The intention of this program is to collate large amounts of unreadable data in an easy to read way.
+Beginning with a CSV file imported from a geneaology website such as ancestry.com or myheritage.com, this CSV file will need to adhere to a standard form
--- test_file.rb
+++ test_file.rb
@@ -1,6 +1,5 @@
 require 'tty-prompt'
 require 'colorize'
-require 'pry'
 
 class Person
     attr_accessor :id, :name, :mat_id, :pat_id
@@ -19,25 +18,15 @@ class Person
     @prompt = TTY::Prompt.new
 
 
-
-def display_all_people(person_array)
-
-        # Just printing
-        person_array.each { |p|
-            puts p.name
-        }
-        
-end
-
 def load_from_file
-        # Read File data into 7 strings
+
         data = []
         File.open(ARGV[0],"r").each { |line|
             data << line.chomp
         }
 
         person_array = []
-        # Split each string data 4 strings, and make a person, and add person to array.
+
         data.each { |i|
             record =  i.split(",")
             this_person = Person.new(record[0],record[1],record[2],record[3])
@@ -57,13 +46,17 @@ def father_menu(person_array)
         p.name == selection
     }
 
-
     father = person_array.find { |p|
         p.id.to_s == found_person.pat_id.to_s
     }
 
     father_name = father.name
+
     puts "#{selection}'s father is #{father_name}"
+
+    # puts "Got an exception, but I'm responding intelligently!"
+
+    main_menu
 
 end
 
@@ -76,41 +69,36 @@ def mother_menu(person_array)
         menu_array << p.name
     }
     selection = @prompt.select("Display All", menu_array)
-    # selection now contains person name "Rhoda"
-    # search the person_array for a person with the name Rhoda
-    # and then get the mat_id from Rhoda
-    # and then search the person_array again for a person with the id == Rhoda's mat_id.
-    person_array = load_from_file
+
     found_person = person_array.find { |p|
         p.name == selection
     }
-    # selection = search_name 
+
     found_person_id = found_person.id
     found_person_mat_id = found_person.mat_id
-    # found_person_name = found_person.name
-    person_array = load_from_file
+   
     mother = person_array.find { |p|
         p.id == found_person.mat_id
     }
     mother_name = mother.name
+
     puts "#{selection}'s mother is #{mother_name}"
+    main_menu
 
 end
 
 def main_menu()
     menu = @prompt.select("Main Menu", 
-    ["1. Display all people", "2. Show maternal lineage", "3. Show paternal lineage", "4. Exit"])
+    ["1. Show maternal lineage", "2. Show paternal lineage", "3. Exit"])
 
     case menu
-        when "1. Display all people"
-            display_all_people(load_from_file)
-        when "2. Show maternal lineage"
+        when "1. Show maternal lineage"
             mother_menu(load_from_file)
-        when "3. Show paternal lineage"
+        when "2. Show paternal lineage"
             father_menu(load_from_file)
-        when "4. Exit"
+        when "3. Exit"
             system 'clear'
-    end
+        end
 end
 
 main_menu
